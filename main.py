e72de7793733d9e6f0481f023877987cae8e5dbd
Soham Sen <76873552+NuclearCactus@users.noreply.github.com>
2022-01-13 16:58:38 +0530

    Added the files

---
+++ main.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+
+with open('home.html', 'r') as html_file:
+    content = html_file.read()
+    # print(content)
+    soup = BeautifulSoup(content, 'lxml')
+    # print(soup.prettify()) # to make the file print more beautifully
+    # tags = soup.find('h5') # searches for only the first element
+    
+    # courses_html_tags = soup.find_all('h5') # searches for every occurence
+    # for course in courses_html_tags:
+        # print(course.text) # text attribute of the tag
+
+    course_cards = soup.find_all('div', class_='card')
+    for course in course_cards:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1] # splits the string into an array
+        print(f'{course_name} costs {course_price}') # print a formatted string
